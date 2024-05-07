02217f9f398f69ecd25841bb43375442bdfb0fea
Sonali Rana <ranasonali987@gmail.com>
2024-05-08 00:37:22 +0530

    feat added: Realtime logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,46 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
+// import socket from "./SocketService";
 import "./App.css";
+import { io } from "socket.io-client";
 
 const App = () => {
   const [grid, setGrid] = useState(Array(9).fill(""));
   const [Xchance, SetXChance] = useState(true); //if true then chance is of X otherwise 0
   const [winner, setWinner] = useState("");
-
+  
+  const socket = useMemo(() => io("http://localhost:8000"), []);
+ 
+  useEffect(() => {
+    socket.on("connect", () => {
+      console.log(socket.id);
+      
+    });
+    socket.on("temp", (message) => {
+      console.log(message);
+    });
+    socket.on("room-event", (message) => {
+      console.log(message);
+    })
+    socket.on("grid-manip", (grid) => {
+      console.log(grid);
+      setGrid(grid);
+    });
+    socket.on("chance-event", (Xchance) => {
+      SetXChance(Xchance);
+    });
+    socket.on("winner-event", (winner) => {
+      setWinner(winner);
+    });
+    return () => {
+      socket.off("connect");
+      socket.off("temp");
+      socket.off("room-event");
+      socket.off("grid-manip");
+      socket.off("chance-event");
+      socket.off("winner-event");
+    };
+  }, [socket]);
+  
   const isDraw = (grid, winner) => {
     if (winner) {
       return false;
@@ -13,75 +48,9 @@ const App = () => {
     const isGridFilled = grid.every((cell) => cell !== "");
     return isGridFilled;
   };
-
-  const gotWinner = (index, updatedGrid) => {
-    let diagnoal1, diagnoal2;
-    const row = Math.floor(index / 3);
-    const col = index % 3;
-    if (index === 0 || index === 4 || index === 8) {
-      diagnoal1 = true;
-    }
-    if (index === 2 || index === 4 || index === 6) {
-      diagnoal2 = true;
-    }
-    if (
-      updatedGrid[row * 3] === updatedGrid[row * 3 + 1] &&
-      updatedGrid[row * 3 + 1] === updatedGrid[row * 3 + 2] &&
-      updatedGrid[row * 3] !== ""
-    ) {
-      return updatedGrid[row * 3];
-    }
-    if (
-      updatedGrid[col] === updatedGrid[col + 3] &&
-      updatedGrid[col + 3] === updatedGrid[col + 6] &&
-      updatedGrid[col] !== ""
-    ) {
-      return updatedGrid[col];
-    }
-    if (diagnoal1) {
-      if (
-        updatedGrid[0] === updatedGrid[4] &&
-        updatedGrid[4] === updatedGrid[8] &&
-        updatedGrid[0] !== ""
-      ) {
-        return updatedGrid[0];
-      }
-    }
-    if (diagnoal2) {
-      if (
-        updatedGrid[2] === updatedGrid[4] &&
-        updatedGrid[4] === updatedGrid[6] &&
-        updatedGrid[2] !== ""
-      ) {
-        return updatedGrid[2];
-      }
-    }
-  };
-  const clickHandler = (index) => {
-    if (winner) {
-      return;
-    }
-    if (grid[index] || winner) return;
-
-    let inputValue = Xchance ? "X" : "0";
-
-    const updatedGrid = grid.map((cell, idx) => {
-      if (idx === index) {
-        return inputValue;
-      }
-      return cell;
-    });
-    setGrid(updatedGrid);
-    SetXChance(!Xchance);
-    const tempWinner = gotWinner(index, updatedGrid);
-    setWinner(tempWinner);
-  };
-  useEffect(() => {
-    const draw = isDraw(grid, winner);
-    if (draw) {
-      return;
-    }
-  }, [grid, winner]);
+  const handleClick = (index) => {
+    socket.emit("click-event",index);
+  }
   return (
     <div className="container">
       <div className="grid">
@@ -94,7 +63,7 @@ const App = () => {
             <button
               key={index}
               className="cells"
-              onClick={() => clickHandler(index)}
+              onClick={() => handleClick(index)}
             >
               {cell}
             </button>
