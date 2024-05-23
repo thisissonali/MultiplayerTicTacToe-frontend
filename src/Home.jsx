5dd42aff1ee1731f268dc71e5f31c5c481ee84ce
Sonali Rana <ranasonali987@gmail.com>
2024-05-24 01:35:04 +0530

    feat: scaling

--- src/Home.jsx
+++ src/Home.jsx
@@ -14,114 +14,117 @@ const Home = () => {
   const [activeChanceBgColorX, setBgColorX] = useState("white");
   const [activeChanceBgColor0, setBgColor0] = useState("white");
 
-  const socket = useMemo(() => io(import.meta.env.VITE_SOCKET_URL || '/'), []);
+  const socket = useMemo(() => io(import.meta.env.VITE_SOCKET_URL || "/"), []);
 
-useEffect(() => {
-  socket.on("connect", () => {
-    console.log(socket.id);
-  });
+  useEffect(() => {
+    socket.on("connect", () => {
+      console.log(socket.id);
+    });
 
-  socket.on("data", (data, newRoom) => {
-    console.log(data[newRoom]);
-    setDataa(data[newRoom]);
-  });
-  socket.on("new-room", (newRoomData) => {
-    setDataa(newRoomData);
-    navigate("/");
-  });
+    socket.on("data", (data, newRoom) => {
+      console.log(data[newRoom]);
+      setDataa(data[newRoom]);
+    });
 
-  if (dataa.chance) {
-    setBgColorX("red");
-    setBgColor0("white");
-  } else {
-    setBgColorX("white");
-    setBgColor0("red");
-  }
+    socket.on("new-room", (newRoomData) => {
+      setDataa(newRoomData);
+      navigate("/");
+    });
 
-  return () => {
-    socket.off("connect");
-    socket.off("data");
-    socket.off("new-room");
+    if (dataa.chance) {
+      setBgColorX("red");
+      setBgColor0("white");
+    } else {
+      setBgColorX("white");
+      setBgColor0("red");
+    }
+    console.log(dataa);
+    return () => {
+      socket.off("connect");
+      socket.off("data");
+      socket.off("new-room");
+    };
+  }, [socket, dataa.chance]);
+
+  const isDraw = (grid, winner) => {
+    if (winner || !grid || grid.length !== 9) {
+      return false;
+    }
+    const isGridFilled = grid.every((cell) => cell !== "");
+    return isGridFilled;
   };
-}, [socket, dataa.chance]);
-
-
-const isDraw = (grid, winner) => {
-  if (winner|| !grid || grid.length !== 9) {
-    return false;
-  }
-  const isGridFilled = grid.every((cell) => cell !== "");
-  return isGridFilled;
-};
 
   const handleClick = (index) => {
     socket.emit("click-event", index);
   };
+
   const handlePlayAgainClick = () => {
     socket.emit("request-new-room", socket.id);
   };
+
   return (
     <div className="container">
-      {!dataa.winner &&
-      !isDraw(dataa.gridVal, dataa.winner) ? (
-        dataa.chance ? (
-          dataa.connecIdsArr.length > 0 &&
-          socket.id === dataa.connecIdsArr[0] && <h1>Your Turn</h1>
-        ) : (
-          dataa.connecIdsArr.length > 0 &&
-          socket.id === dataa.connecIdsArr[1] && <h1>Your Turn</h1>
-        )
+      {dataa.connecIdsArr.length === 0 || dataa.connecIdsArr.length === 1 ? (
+        <div className="msgg">
+          <h1 className="msg-h1">You are alone in the room</h1>
+          <div>Waiting for another player to join...</div>
+        </div>
       ) : (
-        <h1></h1>
-      )}
-
-      <div className="outer">
-        {!dataa.winner &&
-          !isDraw(dataa.gridVal, dataa.winner) && (
-            <div
-              className="chance"
-              style={{ backgroundColor: activeChanceBgColorX }}
-            >
-              X
-            </div>
-          )}
-        <div className="grid">
-          {dataa.winner ? (
-            <div className="result">{`${dataa.winner} wins`}</div>
-          ) : isDraw(dataa.gridVal, dataa.winner) ? (
-            <div className="result">It's a draw!</div>
+        <div className="outer-1">
+          {!dataa.winner && !isDraw(dataa.gridVal, dataa.winner) ? (
+            dataa.chance ? (
+              socket.id === dataa.connecIdsArr[0] && <h1>Your Turn</h1>
+            ) : (
+              socket.id === dataa.connecIdsArr[1] && <h1>Your Turn</h1>
+            )
           ) : (
-            dataa.gridVal.map((cell, index) => (
-              <button
-                key={index}
-                className="cells"
-                onClick={() => handleClick(index)}
+            <h1></h1>
+          )}
+
+          <div className="outer">
+            {!dataa.winner && !isDraw(dataa.gridVal, dataa.winner) && (
+              <div
+                className="chance"
+                style={{ backgroundColor: activeChanceBgColorX }}
               >
-                {cell}
-              </button>
-            ))
+                X
+              </div>
+            )}
+            <div className="grid">
+              {dataa.winner ? (
+                <div className="result">{`${dataa.winner} wins`}</div>
+              ) : isDraw(dataa.gridVal, dataa.winner) ? (
+                <div className="result">It's a draw!</div>
+              ) : (
+                dataa.gridVal.map((cell, index) => (
+                  <button
+                    key={index}
+                    className="cells"
+                    onClick={() => handleClick(index)}
+                  >
+                    {cell}
+                  </button>
+                ))
+              )}
+            </div>
+            {!dataa.winner && !isDraw(dataa.gridVal, dataa.winner) && (
+              <div
+                className="chance"
+                style={{ backgroundColor: activeChanceBgColor0 }}
+              >
+                0
+              </div>
+            )}
+          </div>
+          {(dataa.winner || isDraw(dataa.gridVal, dataa.winner)) && (
+            <button onClick={() => handlePlayAgainClick()} className="play-agn">
+              Find New Match
+            </button>
           )}
         </div>
-        {!dataa.winner &&
-          !isDraw(dataa.gridVal, dataa.winner) && (
-            <div
-              className="chance"
-              style={{ backgroundColor: activeChanceBgColor0 }}
-            >
-              0
-            </div>
-          )}
-      </div>
-      {(dataa.winner || isDraw(dataa.gridVal, dataa.winner)) ? (
-        <button onClick={() => handlePlayAgainClick()} className="play-agn">
-          Play Again
-        </button>
-      ) : (
-        <span></span>
       )}
     </div>
   );
-
 };
 
 export default Home;
